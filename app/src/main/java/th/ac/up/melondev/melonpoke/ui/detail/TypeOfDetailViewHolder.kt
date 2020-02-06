921dd7527ed7e9a43d928df9ee96a6c5d3a6cdbe
Melondev <melondev@icloud.com>
2020-02-06 15:32:00 +0700

    ...

---
+++ app/src/main/java/th/ac/up/melondev/melonpoke/ui/detail/TypeOfDetailViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.examplerecyclerviewtype.main_activity.tab_one_fragment.ViewHolder
+
+class TypeOfDetailViewHolder {
+
+    companion object{
+        const val TYPE_TITLE = 1
+        const val TYPE_IMAGE = 2
+        const val TYPE_INFORMATION = 3
+        const val TYPE_TYPE = 4
+        const val TYPE_UNKNOWN = 5
+    }
+}
